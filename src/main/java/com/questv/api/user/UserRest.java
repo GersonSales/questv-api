3a21c694fe6c42526885c671a6f523972386f1fb
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2019-01-22 23:16:00 -0300

    enable  cors for all requests

--- src/main/java/com/questv/api/user/UserRest.java
+++ src/main/java/com/questv/api/user/UserRest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping()
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserRest {
 
     private final ObjectService<UserDTO> userService;
@@ -18,7 +19,6 @@ public class UserRest {
         assert userService != null;
     }
 
-    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/user")
     @ResponseStatus(value = HttpStatus.CREATED)
     @ResponseBody
@@ -26,25 +26,21 @@ public class UserRest {
         return this.userService.create(userDTO);
     }
 
-    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/user")
     public List<UserDTO> getUsers() {
         return this.userService.findAll();
     }
 
-    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/user/{userId}")
     public UserDTO getUserById(@PathVariable final Long userId) {
         return this.userService.findById(userId);
     }
 
-    @CrossOrigin(origins = "http://localhost:4200", methods = RequestMethod.PUT)
     @PutMapping("/user/{userId}")
     public void putUser(@PathVariable final Long userId, @RequestBody final UserDTO userDTO) {
         this.userService.updateById(userId, userDTO);
     }
 
-    @CrossOrigin(origins = "http://localhost:4200")
     @DeleteMapping("/user/{userId}")
     public void deleteUser(@PathVariable Long userId) {
         this.userService.deleteById(userId);
