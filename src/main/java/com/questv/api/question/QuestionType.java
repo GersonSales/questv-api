70363f6e209b8cbd82529faa2ef9b49c457d0631
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2019-01-25 18:51:42 -0300

    remvoe unecessary code

--- src/main/java/com/questv/api/question/QuestionType.java
+++
@@ -1,9 +0,0 @@
-package com.questv.api.question;
-
-public enum QuestionType {
-  SERIES, SEASON, EPISODE;
-
-  public static QuestionType getValueOf(final String string) {
-      return valueOf(string.toUpperCase());
-  }
-}
--- src/main/java/com/questv/api/series/SeriesRepository.java
+++ src/main/java/com/questv/api/series/SeriesRepository.java
@@ -2,11 +2,7 @@ package com.questv.api.series;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface SeriesRepository extends CrudRepository<SeriesModel, Long> {
-  List<SeriesModel> findByName(final String name);
-
   SeriesModel findById(final long id);
 
 
