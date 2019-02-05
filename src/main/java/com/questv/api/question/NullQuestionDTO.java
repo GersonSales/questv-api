0c3d79e9b53405f02b85441719577155907c87ec
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2019-02-05 04:05:27 -0300

    removing null objects

--- src/main/java/com/questv/api/question/NullQuestionDTO.java
+++
@@ -1,9 +0,0 @@
-package com.questv.api.question;
-
-import java.util.HashMap;
-
-public final class NullQuestionDTO extends QuestionDTO {
-  public NullQuestionDTO() {
-    super(0L, 0L, "NaD",  0, 0, new HashMap<>());
-  }
-}
--- src/main/java/com/questv/api/season/NullSeasonDTO.java
+++
@@ -1,9 +0,0 @@
-package com.questv.api.season;
-
-import java.util.HashSet;
-
-public final class NullSeasonDTO extends SeasonDTO {
-  NullSeasonDTO() {
-    super(0L, 0L, 0, "", new HashSet<>(), new HashSet<>());
-  }
-}
